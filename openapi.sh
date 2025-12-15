2df3254b5ce782f18461335f3b60c8a2a8cf58d6
Ryosuke Sakamoto <sakamoto@sumire-sakamoto.co.jp>
2025-12-15 14:30:58 +0900

    open api

--- openapi.sh
+++ openapi.sh
@@ -1,15 +1,20 @@
 # !/bin/sh
 API_ID=$(docker compose exec aws awslocal apigateway get-rest-apis --query "items[0].id" --output text)
-STAGE_NAME=$(docker compose exec aws awslocal apigateway get-stages --rest-api-id $API_ID --query "item[0].stageName" --output text)
+STAGE=$(docker compose exec aws awslocal apigateway get-stages --rest-api-id $API_ID --query "item[0].stageName" --output text)
 
-BASE_URL="http://localhost:4566/_aws/execute-api/${API_ID}/{basePath}"
+BASE_URL="http://localhost:4566/_aws/execute-api/{apiID}/{basePath}"
 
 docker compose exec aws awslocal apigateway get-export \
     --rest-api-id $API_ID \
-    --stage-name $STAGE_NAME \
+    --stage-name $STAGE \
     --export-type oas30 \
     --accepts application/json \
     temp.json
 
-jq --arg url "$BASE_URL" '.servers[0].url = $url' cdk/temp.json > openapi.json
+jq --arg url "$BASE_URL" \
+    --arg apiid "$API_ID" \
+    '.servers[0].url = $url | .servers[0].variables.apiID.default = $apiid' \
+    cdk/temp.json > openapi.json
+
 rm cdk/temp.json
+
